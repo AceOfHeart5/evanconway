36612d1c11a6be658dc37d78d6d258007d25f18e
AceOfHeart5 <evanconwaymusic@gmail.com>
2021-02-04 02:01:19 -0500

    create first components and stylings

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+const Navbar = () => {
+    return ( 
+        <div className="navbar">
+            <div className="nav blog">
+                Blog
+            </div>
+            <div className="nav projects">
+                Projects
+            </div>
+            <div className="nav about">
+                About
+            </div>
+            <div className="nav contact">
+                Contact
+            </div>
+        </div>
+     );
+}
+ 
+export default Navbar;
