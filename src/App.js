dda3021a0facab8e0db72ddb2d6e238c7eb7da8c
AceOfHeart5 <evanconwaymusic@gmail.com>
2021-02-04 03:08:43 -0500

    add selection bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import Navbar from './Navbar.js';
+import React, { useState } from 'react';
+
+/*
+It's probably foolish to make our own routing system. 
+But who cares, I'm having fun.
+*/
+const getContent = (name) => {
+	if (name === "about") return (
+		<div>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia quos, quaerat, asperiores voluptate praesentium explicabo velit expedita dolorum dolore quisquam libero veritatis repellat? Nemo officiis natus laboriosam, ut magnam iste.</div>
+	);
+	return (
+		<h1>There is no content with name "{name}" :(</h1>
+	);
+}
 
 function App() {
+	const [content, setContent] = useState("none");
 	return (
 		<div className="App">
 			<div className="topbar">
 				<h1 className="my-name">Evan Conway</h1>
-				<Navbar/>
+				<Navbar contentChoice={content} contentSetter={setContent}/>
 			</div>
+			{getContent(content)}
 		</div>
 	);
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,17 +1,20 @@
-const Navbar = () => {
+const Navbar = ({contentChoice, contentSetter}) => {
+
+    const getSelected = (name) => {
+        if (contentChoice === name) return "selected";
+        return "";
+    }
+
     return ( 
         <div className="navbar">
-            <div className="nav blog">
+            <div className={`nav blog ${getSelected("blog")}`} onClick={() => contentSetter("blog")}>
                 Blog
             </div>
-            <div className="nav projects">
+            <div className={`nav projects ${getSelected("projects")}`} onClick={() => contentSetter("projects")}>
                 Projects
             </div>
-            <div className="nav about">
+            <div className={`nav about ${getSelected("about")}`} onClick={() => contentSetter("about")}>
                 About
-            </div>
-            <div className="nav contact">
-                Contact
             </div>
         </div>
      );
