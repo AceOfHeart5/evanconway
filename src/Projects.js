868707d875076bd341e6635860b2e5a6ff26f7f4
AceOfHeart5 <evanconwaymusic@gmail.com>
2021-02-12 00:37:36 -0500

    clean up link css and add more content

--- src/Projects.js
+++ src/Projects.js
@@ -4,7 +4,7 @@ const Projects = () => {
     return (
         <div>
             <section className="project">
-                <a href="https://aceofheart5.github.io/WebMidiLearning/">Piano Site Reading Web App</a>
+                <a className="projectlink" href="https://aceofheart5.github.io/WebMidiLearning/">Piano Site Reading Web App</a>
                 <h3>Web app for practicing sight reading with an electric piano.</h3>
                 <img src={IMAGES.sitereadingapp} alt="pic of sheet music app"/>
                 <main>
@@ -16,9 +16,9 @@ const Projects = () => {
                 </main>
             </section>
             <section className="project">
-                <a href="https://gla55world.itch.io/the-evil-tower">Game Maker Studio Project</a>
+                <a className="projectlink" href="https://gla55world.itch.io/the-evil-tower">Game Maker Studio Project</a>
                 <h3>A short side scroller action platformer made in Game Maker Studio 2.</h3>
-                <img src={IMAGES.eviltower01} alt="pic of a video game" />
+                <img src={IMAGES.eviltower.one} alt="pic of a video game" />
                 <main>
                     <p>
                         My main objective when creating this game was to uncover all the unexpected challenges involved in making a complete game. The content of this game is minimal, but 
@@ -40,7 +40,15 @@ const Projects = () => {
                 </main>
             </section>
             <section className="project">
-                <a href="https://www.licenselab.com/search/tracks/list/k_conway/">Music</a>
+                <a className="projectlink" href="https://www.licenselab.com/search/tracks/list/k_conway/">Music</a>
+                <h3>250+ pieces of music I have writen for TV, radio, and various media.</h3>
+                <p>
+                    Each song is part of an album, and each album fits a specific theme or mood. Some of my favorites are <a href="https://www.licenselab.com/track/8c1c1ca2bc3c191f/beautiful-entanglement">Beautiful Entanglement</a>, <a href="https://www.licenselab.com/track/1d98ce3e652151e5/nuts-and-bolts">Nuts And Bolts</a>, <a href="https://www.licenselab.com/track/9c79172e951d2ebb/glorious-dawn">Glorious Dawn</a>, and <a href="https://www.licenselab.com/track/d537ecbc9ed581ad/adorable-saves-the-day">Adorable Saves The Day</a>.
+                </p>
+            </section>
+            <section className="project">
+                <h1>This Website</h1>
+                <p>This site was made with React. Honestly React is waaaaaaay overkill for this site, but I wanted more experience with React, and what better way to do that than to make something.</p>
             </section>
         </div>
     );
--- src/assets/blogs.js
+++ src/assets/blogs.js
@@ -22,15 +22,14 @@ const BLOGS = [
         description: "Discovering a love for programming, and uncovering all the challenges of making a feature complete video game.",
         body: <article>
             <h1>The Evil Tower</h1>
-            <a href="https://gla55world.itch.io/the-evil-tower">
-                <img src={IMAGES.eviltower.one} alt="guy swinging sword"/>
-            </a>
-            <a href="https://gla55world.itch.io/the-evil-tower">
-                <img src={IMAGES.eviltower.two} alt="standing on ladder"/>
-            </a>
+            <img src={IMAGES.eviltower.one} alt="guy swinging sword"/>
+            <img src={IMAGES.eviltower.two} alt="standing on ladder"/>
+            <br/>
+            <a href="https://gla55world.itch.io/the-evil-tower">Link to the game.</a>
             <p>
                 There have been a number of times in my life where I've tried to make a video game, or get into game programming. But not being a programmer, and not really knowing 
-                where to look for help, I've always gotten frustrated and given up. 
+                where to look for help, I've always gotten frustrated and given up. It wasn't until Googling "What was Undertale made with?" and trying Game Maker Studio that I was 
+                able to really dive into programming. Every tool has a different amount of abstraction, and I think Game Maker has the perfect amount for me. 
             </p>
         </article>
     }
