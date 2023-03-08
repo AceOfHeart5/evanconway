2969e97ff7bf10f77f0bf9ed2d14ae2d54cbd4ef
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-03-07 22:17:49 -0500

    update about section

--- src/About.js
+++ src/About.js
@@ -1,28 +1,40 @@
 const About = () => {
     const noBottom = { marginBottom: "0px"};
     const smallTop = { marginTop: "5px"};
+
     return (
         <article style={{ maxWidth: "900px" }}>
             <p style={{paddingBottom: "10px", borderColor: "grey", borderBottomStyle: "solid"}}>
-                Evan Conway is a composer and programmer based in Rochester, NY. He writes for the Modus Operandi music library and has been pursuing 
+                Evan Conway is a composer and programmer based in Brooklyn, NY. He wrote music for the Modus Operandi music library and has been pursuing 
                 programming professionally since 2018. In his spare time, he writes games with GameMaker Studio 2.
             </p>
             <h1>Experience</h1>
+            <h4 style={noBottom}>Junior Software Engineer</h4>
+            <h5 style={smallTop}>Zverse/Revaly, October 2021 to Present</h5>
+            <ul>
+                <li>Assisted building Revaly, a real-time multiplayer 3D design review web app (comparable to Google Docs + Github + Miro for 3D)</li>
+                <li>Wrote frontend tests using Playwright and integrated them into the main repo as a Github workflow</li>
+                <li>Made dozens of React components using Typescript, MUI, CSS, Tailwind, Redux, and Apollo GraphQL</li>
+                <li>Used data structures and algorithms to optimize manipulating individual sections of complex models</li>
+                <li>Integrated permissions data with frontend, and wrote Supabase migration for PostgreSQL function to give accounts content control over hundreds of users</li>
+                <li>Built convenient wrapper functions for logging user interaction to Azure app-insights, simplifying tracking user workflow</li>
+                <li>Worked directly with the Director of UX to improve accessibility through aria labels, correct semantic tags, and mui-tooltips</li>
+            </ul>
             <h4 style={noBottom}>Software Engineering Intern</h4>
-            <h5 style={smallTop}>June 2021 to Present</h5>
+            <h5 style={smallTop}>co:census, June 2021 to September 2021</h5>
             <ul>
                 <li>Creates chatbots with Twilio Studio</li>
                 <li>Automates creation of Twilio Flows using Go</li>
                 <li>Created custom JSON format to represent sms surveys</li>
             </ul>
             <h4 style={noBottom}>Composer</h4>
-            <h5 style={smallTop}>June 2011 to Present</h5>
+            <h5 style={smallTop}>June 2011 to October 2021</h5>
             <ul>
-                <li>Composes stock music for Modus Operandi music library</li>
+                <li>Composed stock music for Modus Operandi music library</li>
                 <li>Has pieces aired on Fox, CBS, NBC, and globally</li>
             </ul>
             <h4 style={noBottom}>Participant at the Recurse Center</h4>
-            <h5 style={smallTop}>July 2020 to September 2020</h5>
+            <h5 style={smallTop}>Recurse Center, July 2020 to September 2020</h5>
             <ul>
                 <li>Learned JavaScript, HTML, and CSS independently</li>
                 <li>Learned abcjs music notation library to display sheet music in HTML</li>
@@ -51,6 +63,6 @@ const About = () => {
             <h5 style={smallTop}>Lawrence University, June 2013</h5>
         </article>
     );
-}
+};
  
 export default About;
