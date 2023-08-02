47c375c1a37794c5e1176d29deb93f35c72d0070
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-08-02 03:09:32 -0400

    add experience component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,8 @@
+import Experience from "./Experience";
+
 const About = () => {
-    const noBottom = { marginBottom: "0px"};
-    const smallTop = { marginTop: "5px"};
+    const noBottom = { marginBottom: "0px" };
+    const smallTop = { marginTop: "5px" };
 
     return (
         <article style={{ maxWidth: "900px" }}>
@@ -9,54 +11,77 @@ const About = () => {
                 programming professionally since 2018. In his spare time, he writes games with GameMaker Studio 2.
             </p>
             <h1>Experience</h1>
-            <h4 style={noBottom}>Junior Software Engineer</h4>
-            <h5 style={smallTop}>Zverse/Revaly, October 2021 to Present</h5>
-            <ul>
-                <li>Assisted building Revaly, a real-time multiplayer 3D design review web app (comparable to Google Docs + Github + Miro for 3D)</li>
-                <li>Wrote frontend tests using Playwright and integrated them into the main repo as a Github workflow</li>
-                <li>Made dozens of React components using Typescript, MUI, CSS, Tailwind, Redux, and Apollo GraphQL</li>
-                <li>Used data structures and algorithms to optimize manipulating individual sections of complex models</li>
-                <li>Integrated permissions data with frontend, and wrote Supabase migration for PostgreSQL function to give accounts content control over hundreds of users</li>
-                <li>Built convenient wrapper functions for logging user interaction to Azure app-insights, simplifying tracking user workflow</li>
-                <li>Worked directly with the Director of UX to improve accessibility through aria labels, correct semantic tags, and mui-tooltips</li>
-            </ul>
-            <h4 style={noBottom}>Software Engineering Intern</h4>
-            <h5 style={smallTop}>co:census, June 2021 to September 2021</h5>
-            <ul>
-                <li>Created chatbots with Twilio Studio</li>
-                <li>Automated creation of Twilio Flows using Go</li>
-                <li>Created custom JSON format to represent sms surveys</li>
-            </ul>
-            <h4 style={noBottom}>Composer</h4>
-            <h5 style={smallTop}>June 2011 to October 2021</h5>
-            <ul>
-                <li>Composed stock music for Modus Operandi music library</li>
-                <li>Has pieces aired on Fox, CBS, NBC, and globally</li>
-            </ul>
-            <h4 style={noBottom}>Participant at the Recurse Center</h4>
-            <h5 style={smallTop}>Recurse Center, July 2020 to September 2020</h5>
-            <ul>
-                <li>Learned JavaScript, HTML, and CSS independently</li>
-                <li>Learned abcjs music notation library to display sheet music in HTML</li>
-                <li>Created JavaScript classes to model pitches and chords and randomly generate notes</li>
-                <li>Created a music sight reading app</li>
-                <li>Created GameMaker text boxes with string parser to customize text appearance</li>
-                <li>Created movement code for 2D games using Bresenham's line llgorithm</li>
-                <li>Solved LeetCode questions in daily virtual pairing sessions with experienced devs</li>
-                <li>Led morning check-ins and MC'd weekly music listening group</li>
-                <li>Attended virtual programming lectures on machine learning, frontend web dev, and more</li>
-            </ul>
-            <h4 style={noBottom}>Piano Teacher</h4>
-            <h5 style={smallTop}>Hartland Music, September 2013 to June 2018</h5>
-            <ul>
-                <li>Gave lessons to students aged 6 to 65</li>
-                <li>Organized studio events, recitals, and competition entrances</li>
-            </ul>
-            <h4 style={noBottom}>Programmer</h4>
-            <h5 style={smallTop}>Zerrez Solutions, November 2014 to July 2015</h5>
-            <ul>
-                <li>Assisted in building a 2D modeling application in JavaScript and C++/QT</li>
-            </ul>
+            <Experience
+                title="Junior Software Engineer"
+                company="Zverse/Revaly"
+                start={{ month: "October", year: 2021 }}
+                end={{ month: "March", year: 2023 }}
+                activities={[
+                    "Assisted building Revaly, a real-time multiplayer 3D design review web app (comparable to Google Docs + Github + Miro for 3D)",
+                    "Wrote frontend tests using Playwright and integrated them into the main repo as a Github workflow",
+                    "Made dozens of React components using Typescript, MUI, CSS, Tailwind, Redux, and Apollo GraphQL",
+                    "Used data structures and algorithms to optimize manipulating individual sections of complex models",
+                    "Integrated permissions data with frontend, and wrote Supabase migration for PostgreSQL function to give accounts content control over hundreds of users",
+                    "Built convenient wrapper functions for logging user interaction to Azure app-insights, simplifying tracking user workflow",
+                    "Worked directly with the Director of UX to improve accessibility through aria labels, correct semantic tags, and mui-tooltips",
+                ]}
+            />
+            <Experience
+                title="Software Engineer Intern"
+                company="co:census"
+                start={{ month: "June", year: 2021 }}
+                end={{ month: "September", year: 2021 }}
+                activities={[
+                    "Created chatbots with Twilio Studio",
+                    "Automated creation of Twilio Flows using Go",
+                    "Created custom JSON format to represent sms surveys",
+                ]}
+            />
+            <Experience
+                title="Composer"
+                start={{ month: "June", year: 2011 }}
+                end={{ month: "October", year: 2021 }}
+                activities={[
+                    "Composed stock music for Modus Operandi music library",
+                    "Has pieces aired on Fox, CBS, NBC, and globally",
+                ]}
+            />
+            <Experience
+                title="Participant at the Recurse Center"
+                company="Recurse Center"
+                start={{ month: "July", year: 2020 }}
+                end={{ month: "September", year: 2020 }}
+                activities={[
+                    "Learned JavaScript, HTML, and CSS independently",
+                    "Learned abcjs music notation library to display sheet music in HTML",
+                    "Created JavaScript classes to model pitches and chords and randomly generate notes",
+                    "Created a music sight reading app",
+                    "Created GameMaker text boxes with string parser to customize text appearance",
+                    "Created movement code for 2D games using Bresenham's line llgorithm",
+                    "Solved LeetCode questions in daily virtual pairing sessions with experienced devs",
+                    "Led morning check-ins and MC'd weekly music listening group",
+                    "Attended virtual programming lectures on machine learning, frontend web dev, and more",
+                ]}
+            />
+            <Experience
+                title="Piano Teacher"
+                company="Hartland Music"
+                start={{ month: "September", year: 2013 }}
+                end={{ month: "June", year: 2018 }}
+                activities={[
+                    "Gave lessons to students aged 6 to 65",
+                    "Organized studio events, recitals, and competition entrances",
+                ]}
+            />
+            <Experience
+                title="Programmer"
+                company="Zerrez Solutions"
+                start={{ month: "November", year: 2014 }}
+                end={{ month: "July", year: 2015 }}
+                activities={[
+                    "Assisted in building a 2D modeling application in JavaScript and C++/QT",
+                ]}
+            />
             <h4 style={noBottom}>Associate in Science, Computer Science</h4>
             <h5 style={smallTop}>Monroe Community College, June 2020</h5>
             <h4 style={noBottom}>Bachelor of Music, Piano Performance</h4>
---
+++ src/components/Experience.tsx
@@ -0,0 +1,28 @@
+type Month = "January" | "February" | "March" | "April" | "May" | "June" | "July" | "August" | "September" | "October" | "November" | "December";
+interface CompanyDate {
+    month: Month,
+    year: number,
+}
+
+
+interface ExperienceProps {
+    title: string,
+    company?: string,
+    start: CompanyDate,
+    end?: CompanyDate,
+    activities: string[],
+}
+
+const Experience = ({ title, company, start, end, activities }: ExperienceProps) => {
+    const displayCompany = company ? `${company}, ` : "";
+    const dateStart = `${start.month} ${start.year}`;
+    const dateEnd = end === undefined ? "present" : `${end.month} ${end.year}`;
+
+    return <>
+        <h4 style={{ marginBottom: "0px" }}>{ title }</h4>
+        <h5 style={{ marginTop: "5px", marginBottom: 0 }}>{`${displayCompany} ${dateStart} to ${dateEnd}`}</h5>
+        <ul style={{ marginTop: "8px" }}>{activities.map(activity => <li>{activity}</li>)}</ul>
+    </>;
+};
+
+export default Experience;
