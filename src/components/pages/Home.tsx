a48fe0173bdc2b26b3265d0722341f9722bd74cf
Evan Conway <evanconwaymusic@gmail.com>
2023-09-21 18:38:55 -0400

    change homepage to markdown

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,21 +1,21 @@
-import { Typography, Link, Container } from "@mui/material";
+import { useState, useEffect } from "react";
+import Markdown from 'markdown-to-jsx';
+import content from "../../content/home.md";
 
 const Home = () => {
+    const [markdown, setMarkdown] = useState("");
+    
+    useEffect(() => {
+        const setupContent = async () => {
+            const res = await fetch(content);
+            const text = await res.text();
+            setMarkdown(text);
+        };
+        setupContent();
+    }, []);
+
     return (
-        <Container>
-            <Typography variant="h3">About Me</Typography>
-            <Typography>
-            I'm a programmer and composer based in Brooklyn NY. Whether it's a new library, an interesting VST, or game, I love learning new things. I spent 10 years in the music industry as a piano teacher and composer. And I've spent about 1.5 years programming professionally.  I value clean, readable code, and seeing a project done well. 
-            </Typography>
-            <Typography>
-                If you'd like to know more check out my demo projects linked above!
-            </Typography>
-            <Typography>
-                I'm currently job searching. If you're interested in contacting me, please reach out. I'd love to hear from you!
-            </Typography>
-            <Typography>evanconwaymusic@gmail.com</Typography>     
-            <Link href="https://docs.google.com/document/d/1qRQ8ZiwPFS40rOGruCJJj6VH0nnWQdo6qOWGVO3WV4g/preview">Resume</Link>
-        </Container>
+        <Markdown>{markdown}</Markdown>
     );
 };
 
---
+++ src/content/home.md
@@ -0,0 +1,10 @@
+### About Me
+
+I'm a programmer and composer based in Brooklyn NY. Whether it's a new library, an interesting VST, or game, I love learning new things. I spent 10 years in the music industry as a piano teacher and composer. And I've spent about 1.5 years programming professionally.  I value clean, readable code, and seeing a project done well. 
+
+If you'd like to know more check out my demo projects linked above!
+
+I'm currently job searching. If you're interested in contacting me, please reach out. I'd love to hear from you!
+
+evanconwaymusic@gmail.com  
+[resume](https://docs.google.com/document/d/1qRQ8ZiwPFS40rOGruCJJj6VH0nnWQdo6qOWGVO3WV4g/preview)
