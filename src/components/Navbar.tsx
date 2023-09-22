9c0a8d2241f6154cbe3cdece19a19d7eb0e1ae18
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-09-21 23:49:47 -0400

    replace github icon with mui icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import githubLogo from '../assets/github/github-mark.svg';
-import { Link, Button, Stack } from '@mui/material';
+import { Button, Stack, IconButton } from '@mui/material';
+import { GitHub } from '@mui/icons-material';
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -12,9 +12,9 @@ const Navbar = () => {
     return <Stack direction="row" alignItems={"center"}>
         <Button onClick={() => setPage("")}>Home</Button>
         <Button onClick={() => setPage("projects")}>Projects</Button>
-        <Link href="https://github.com/AceOfHeart5">
-            <img alt="GitHub Logo" style={{ width: "32px", height: "32px" }} src={githubLogo}/>
-        </Link>
+        <IconButton color="primary" href="https://github.com/AceOfHeart5">
+            <GitHub fontSize='large'/>
+        </IconButton>
     </Stack>;
 }
 
